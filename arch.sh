8deecd3d8e934372d2f2e43a36b461d165e448f9
Paul Pleskunov <pp@positroncloud.com>
2025-02-09 17:36:59 -0500

    Updated packages list and main install script

--- arch.sh
+++ arch.sh
@@ -15,7 +15,7 @@
 # the Free Software Foundation; either version 3 of the License, or (at
 # your option) any later version.
 #
-# lumapi_connector is distributed in the hope that it will be useful, but
+# arch.sh script is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # General Public License for more details.
